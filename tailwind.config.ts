becac3a4a36ac143eb1bec530887b5ebb7283693
umer394 <umershhzad7@gmail.com>
2025-05-08 12:15:17 +0500

    header push

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+export default {
+    content: [
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    ]
+}
